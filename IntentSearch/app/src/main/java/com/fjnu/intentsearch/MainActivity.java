9aa80474d1ddb83022991b805e70cfd600cf5210
zhuzhongce <24506033382qq.com>
2017-12-28 15:57:20 +0800

    browser and search

---
+++ IntentSearch/app/src/main/java/com/fjnu/intentsearch/MainActivity.java
@@ -0,0 +1,34 @@
+package com.fjnu.intentsearch;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editUrl;
+    Button btnBrowse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+
+        editUrl = (EditText)findViewById(R.id.edit_url);
+        btnBrowse = (Button)findViewById(R.id.btn_browse);
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editUrl.getText().toString();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
